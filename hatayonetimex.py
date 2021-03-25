92758ff97f0871e3284f6569d6caa2b2b7fabaf3
xemretanriverdi <xemretanriverdi@gmail.com>
2021-03-26 02:39:12 +0300

    Hata Yonetimi

---
+++ hatayonetimex.py
@@ -0,0 +1,24 @@
+#hata yonetimi ornekleri 
+
+# girilen parolada türkçe karakter varsa belirt
+
+
+
+while True:
+    deger = input("sayi giriniz: ")
+
+    try:
+        result=(float(deger))
+
+    except ValueError:
+        print("Boyle bi sayımı var amk")
+        continue
+    break
+
+
+
+
+
+
+  
+
